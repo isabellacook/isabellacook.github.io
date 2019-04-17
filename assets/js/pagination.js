184217ae73939f737e1c285795b068a6cfbc879d
isabellacook <40841096+isabellacook@users.noreply.github.com>
2019-04-16 18:19:07 -0700

    Update assets

---
+++ assets/js/pagination.js
@@ -0,0 +1,29 @@
+$('#pagination-leafbuyer').twbsPagination({
+  totalPages: 15,
+  next: 'Next',
+  prev: 'Prev',
+  
+  onPageClick: function (event, page) {
+    $('#page-content-leafbuyer').load("../../templates/leafbuyer.html #articles-leafbuyer-" + page);
+  }
+});
+
+$('#pagination-calaveras').twbsPagination({
+  totalPages: 4,
+  next: 'Next',
+  prev: 'Prev',
+  
+  onPageClick: function (event, page) {
+    $('#page-content-calaveras').load("../../templates/calaveras.html #articles-calaveras-" + page);
+  }
+});
+
+$('#pagination-fishrap').twbsPagination({
+  totalPages: 4,
+  next: 'Next',
+  prev: 'Prev',
+  
+  onPageClick: function (event, page) {
+    $('#page-content-fishrap').load("../../templates/fishrap.html #articles-fishrap-" + page);
+  }
+});
